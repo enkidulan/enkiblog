49f36a6179809055717c6945a28d98c1d5125102
Maksym Shalenyi <supamaxy@gmail.com>
2017-08-05 13:57:20 +0300

    Updated workflow

---
+++ src/enkiblog/core/workflow.py
@@ -0,0 +1,59 @@
+from repoze.workflow import Workflow
+from inspect import signature
+from collections import namedtuple
+
+
+P = namedtuple('Permission', ('allowance', 'agents', 'actions'))
+
+
+def prop(prop):
+    def getter(instance):
+        return getattr(instance, prop)
+    return getter
+
+
+class PropsProxy:
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+
+class State(PropsProxy):
+    pass
+
+
+class Transition(PropsProxy):
+    pass
+
+
+class WorkflowBuilder:
+    __baseclass = Workflow
+
+    def __new__(cls):
+        init_params = {n: getattr(cls, n) for n in signature(cls.__baseclass).parameters}
+        states = {n: a for n, a in cls.__dict__.items() if isinstance(a, State)}
+        transitions = {n: a for n, a in cls.__dict__.items() if isinstance(a, Transition)}
+        members = {
+            n: a for n, a in cls.__dict__.items()
+            if not(n.startswith('_') or n in [*init_params, *states, *transitions])}
+        members['states'] = type(
+            cls.__name__ + 'States', (object, ), {i: i for i in states})
+        reverse_states = {v: k for k, v in states.items()}
+        init_params['initial_state'] = reverse_states[init_params['initial_state']]
+
+        workflow_cls = type(cls.__name__, (cls.__baseclass, ), members)
+
+        workflow = workflow_cls(**init_params)
+
+        for name, attr in states.items():
+            workflow.add_state(name, *attr.args, **attr.kwargs)
+
+        for name, attr in transitions.items():
+            args = [reverse_states[i] if isinstance(i, State) else i for i in attr.args]
+            kwargs = {
+                k: reverse_states[v] if isinstance(v, State) else v
+                for k, v in attr.kwargs.items()}
+            workflow.add_transition(name, *args, **kwargs)
+
+        workflow.check()
+        return workflow
--- src/enkiblog/workflow.py
+++ src/enkiblog/workflow.py
@@ -1,53 +1,41 @@
-from repoze.workflow import Workflow
 from pyramid.security import Allow, Everyone, DENY_ALL
-from collections import namedtuple
-
-P = namedtuple('Permission', ('allowance', 'agents', 'actions'))
+from enkiblog.core.workflow import WorkflowBuilder, State, Transition, P, prop
 
 
-def prop(prop):
-    def getter(instance):
-        return getattr(instance, prop)
-    return getter
+class SimpleWorkflow(WorkflowBuilder):
+    """ Simple workflow with only public/private states """
 
+    name = 'simple_publication'
+    state_attr = 'state'
+    description = ''
+    permission_checker = None
 
-def simple_workflow():
-    public = 'public'
-    workflow = Workflow(
-        state_attr='state',
-        initial_state='private',
-        name='simple_publication',
-        permission_checker=None,
-    )
     permissions = {
         'viewing': ('view',),
-        'managing': ('view', 'edit'),  # that looks a bit too much, websauna does it, should I integrate it into websauna? - should admin panel resources use it?
-    }
-    workflow.add_state(
-        public,
+        'managing': ('view', 'edit')}
+
+    public = State(
         acl=(P(Allow, (Everyone, ), permissions['viewing']),
              P(Allow, prop('editors'), permissions['managing']),
-             P(*DENY_ALL))
-    )
-    workflow.add_state(
-        'private',
+             P(*DENY_ALL)))
+    private = State(
         acl=(P(Allow, prop('editors'), permissions['managing']),
-             P(*DENY_ALL))
-    )
-    # TODO: make use of transactions
-    workflow.add_transition('publish', 'private', 'public')
-    workflow.add_transition('hide', 'public', 'private')
-    # XXX:
-    # TODO: add test that unpublished posts have a mark
-    workflow.is_published = lambda context: getattr(context, workflow.state_attr, None) == public
-    workflow.check()
-    return workflow
+             P(*DENY_ALL)))
+
+    initial_state = private
+
+    publish = Transition(private, public)
+    hide = Transition(public, private)
+
+    def is_published(self, context):
+        """ checks if context is published and returns bool result """
+        return getattr(context, self.state_attr, None) == self.states.public
 
 
 def get_worflow(request):
     workflow = getattr(request.registry, "workflow", None)
     if workflow is None:
-        workflow = request.registry.workflow = simple_workflow()
+        workflow = request.registry.workflow = SimpleWorkflow()
     return workflow
 
 
